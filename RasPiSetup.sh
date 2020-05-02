eb4e3210328f6c26db2258927cf34ab38adf2250
Chris Hamilton <chamilton6983@gmail.com>
2020-05-02 20:40:08 +0100

    Update RasPiSetup.sh

--- RasPiSetup.sh
+++ RasPiSetup.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 
 # To run this script, use the following command at a terminal prompt:
-# sudo wget -O - https://raw.githubusercontent.com/chamilton6983/RasPiSetup/master/RasPiSetup.sh | bash
+# sudo wget - https://raw.githubusercontent.com/chamilton6983/RasPiSetup/master/RasPiSetup.sh | bash
 
 #Updates
-#echo "-----Updating and Upgrading-----"
-#apt-get update
-#apt-get upgrade
+sudo echo "-----Updating and Upgrading-----"
+sudo apt-get update
+sudo apt-get upgrade
 
 #Change Hostname
-echo "-----Changing Hostname-----"
-read -p "Enter new Hostname: " hostname	#Ask the user for the new hostname
-sed -i "s/raspberrypi/$hostname/g" /etc/hostname	#Replaces the hostname in the /etc/hostname file
-sed -i "s/raspberrypi/$hostname/g" /etc/hosts	#Replaces the hostname in the /etc/hosts file
+sudo echo "-----Changing Hostname-----"
+sudo read -p "Enter new Hostname: " hostname	#Ask the user for the new hostname
+sudo sed -i "s/raspberrypi/$hostname/g" /etc/hostname	#Replaces the hostname in the /etc/hostname file
+sudo sed -i "s/raspberrypi/$hostname/g" /etc/hosts	#Replaces the hostname in the /etc/hosts file
 
-#New Username
-echo "-----Adding new admin account-----"
-read -p "Enter username : " username
-read -s -p "Enter password : " password
-egrep "^$username" /etc/passwd >/dev/null
-if [ $? -eq 0 ]; then
-	echo "$username exists!"
-	exit 1
-else
-	pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-	useradd -m -p "$pass" "$username"
-	[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-fi
-usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $username	#Add the new user to the required groups
-echo -e "$username ALL=(ALL) PASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd	#Appends the new user to the end of the nopasswd list and requires the password to be entered for sudo activities
-echo -e "AllowUsers $username" >> /etc/ssh/sshd_config	#Append the new user to the end of the sshd_config file to allow sshd connections
-systemctl restart ssh	#restart ssh to pick previous line
+#New admin account
+sudo echo "-----Adding new admin account-----"
+sudo read -p "Enter username : " username
+sudo read -s -p "Enter password : " password
+sudo egrep "^$username" /etc/passwd >/dev/null
+sudo if [ $? -eq 0 ]; then
+	sudo echo "$username exists!"
+	sudo exit 1
+sudo else
+	sudo pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+	sudo useradd -m -p "$pass" "$username"
+	sudo [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+sudo fi
+sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $username	#Add the new user to the required groups
+sudo echo -e "$username ALL=(ALL) PASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd	#Appends the new user to the end of the nopasswd list and requires the password to be entered for sudo activities
+sudo echo -e "AllowUsers $username" >> /etc/ssh/sshd_config	#Append the new user to the end of the sshd_config file to allow sshd connections
+sudo systemctl restart ssh	#restart ssh to pick previous line
 
 #Harden the pi account
-echo "-----Hardening pi account-----"
-read -p "Enter new password for pi" pipassword
-echo -e "pi:$pipassword" | chpasswd	#use the chpasswd util to change to pi user password
-passwd pi -l	#Disable the pi account
+sudo echo "-----Hardening pi account-----"
+sudo read -p "Enter new password for pi" pipassword
+sudo echo -e "pi:$pipassword" | chpasswd	#use the chpasswd util to change to pi user password
+sudo passwd pi -l	#Disable the pi account
 
 #Change root password
-echo "-----Hardening root account-----"
-read -p "Enter new password for root" rootpassword
-echo -e "root:$rootpassword" | chpasswd	#use the chpasswd util to change to root user password
+sudo echo "-----Hardening root account-----"
+sudo read -p "Enter new password for root" rootpassword
+sudo echo -e "root:$rootpassword" | chpasswd	#use the chpasswd util to change to root user password
 
 #Hardening
-echo "-----Other hardening actions-----"
-sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd	#Ensure all accounts need to enter their password when carrying out sudo
+sudo echo "-----Other hardening actions-----"
+sudo sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd	#Ensure all accounts need to enter their password when carrying out sudo
+
+#Application Installation
+sudo echo "-----Installing applications-----"
+sudo apt-get install cockpit
