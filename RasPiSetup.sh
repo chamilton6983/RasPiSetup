1cbc9d7bf73b56ac0d5522bd113164d5cba4dbd9
Chris Hamilton <chamilton6983@gmail.com>
2020-05-02 16:27:52 +0100

    Update RasPiSetup.sh

--- RasPiSetup.sh
+++ RasPiSetup.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 # To run this script, use the following command at a terminal prompt:
-# wget -O - https://raw.githubusercontent.com/chamilton6983/RasPiSetup/master/RasPiSetup.sh | bash
+# sudo wget -O - https://raw.githubusercontent.com/chamilton6983/RasPiSetup/master/RasPiSetup.sh | bash
 
 #Updates
 echo "Updating and Upgrading"
-sudo apt-get update
-sudo apt-get upgrade
+apt-get update
+apt-get upgrade
 
 #Change root password
 echo "Changing the root password"
-sudo passwd root
+passwd root
 
 #Switch to root
 echo "Switching to root account"
-sudo su
+su
 
 #Change Hostname
 echo "Changing hostname"
 read -p "Enter New Hostname: " hostname	#Ask the user for the new hostname
-sudo sed -i "s/raspberrypi/$hostname/g" /etc/hostname	#Replaces the hostname in the /etc/hostname file
-sudo sed -i "s/raspberrypi/$hostname/g" /etc/hosts	#Replaces the hostname in the /etc/hosts file
+sed -i "s/raspberrypi/$hostname/g" /etc/hostname	#Replaces the hostname in the /etc/hostname file
+sed -i "s/raspberrypi/$hostname/g" /etc/hosts	#Replaces the hostname in the /etc/hosts file
 
 #New Username
 echo "adding new user"
 read -p "Enter New Username: " username	#Ask the user for the new username
-sudo adduser $username	#Create the new user
-sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $username	#Add the new user to the required groups
-sudo echo -e "$username ALL=(ALL) PASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd	#Appends the new user to the end of the nopasswd list and requires the password to be entered for sudo activities
-sudo echo -e "AllowUsers $username" >> /etc/ssh/sshd_config	#Append the new user to the end of the sshd_config file to allow sshd connections
-sudo systemctl restart ssh	#restart ssh to pick previous line
+adduser $username	#Create the new user
+usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $username	#Add the new user to the required groups
+echo -e "$username ALL=(ALL) PASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd	#Appends the new user to the end of the nopasswd list and requires the password to be entered for sudo activities
+echo -e "AllowUsers $username" >> /etc/ssh/sshd_config	#Append the new user to the end of the sshd_config file to allow sshd connections
+systemctl restart ssh	#restart ssh to pick previous line
 
 #Harden the pi account
 echo "pi account hardening"
-sudo passwd	#Change the pi user account password
-sudo passwd pi -l	#Disable the pi account
+passwd	#Change the pi user account password
+passwd pi -l	#Disable the pi account
 
 #Hardening
 echo "Other hardening actions"
-sudo sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd	#Ensure all accounts need to enter their password when carrying out sudo
+sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd	#Ensure all accounts need to enter their password when carrying out sudo
