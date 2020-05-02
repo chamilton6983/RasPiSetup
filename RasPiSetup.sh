e7ca15d7ae5dee38adbefaf71367bc09d21f5bb7
Chris Hamilton <chamilton6983@gmail.com>
2020-05-02 20:26:00 +0100

    Update RasPiSetup.sh

--- RasPiSetup.sh
+++ RasPiSetup.sh
@@ -4,34 +4,45 @@
 # sudo wget -O - https://raw.githubusercontent.com/chamilton6983/RasPiSetup/master/RasPiSetup.sh | bash
 
 #Updates
-echo "Updating and Upgrading"
+echo "-----Updating and Upgrading-----"
 apt-get update
 apt-get upgrade
 
-#Change root password
-echo "Changing the root password"
-passwd root
-
 #Change Hostname
-echo "Changing hostname"
-read -p "Enter New Hostname: " hostname	#Ask the user for the new hostname
+echo "-----Changing Hostname-----"
+read -p "Enter new Hostname: " hostname	#Ask the user for the new hostname
 sed -i "s/raspberrypi/$hostname/g" /etc/hostname	#Replaces the hostname in the /etc/hostname file
 sed -i "s/raspberrypi/$hostname/g" /etc/hosts	#Replaces the hostname in the /etc/hosts file
 
 #New Username
-echo "adding new user"
-read -p "Enter New Username: " username	#Ask the user for the new username
-adduser $username	#Create the new user
+echo "-----Adding new admin account-----"
+read -p "Enter username : " username
+read -s -p "Enter password : " password
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	echo "$username exists!"
+	exit 1
+else
+	pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+	useradd -m -p "$pass" "$username"
+	[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+fi
 usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $username	#Add the new user to the required groups
 echo -e "$username ALL=(ALL) PASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd	#Appends the new user to the end of the nopasswd list and requires the password to be entered for sudo activities
 echo -e "AllowUsers $username" >> /etc/ssh/sshd_config	#Append the new user to the end of the sshd_config file to allow sshd connections
 systemctl restart ssh	#restart ssh to pick previous line
 
 #Harden the pi account
-echo "pi account hardening"
-passwd	#Change the pi user account password
+echo "-----Hardening pi account-----"
+read -p "Enter new password for pi" pipassword
+echo -e "pi:$pipassword" | chpasswd	#use the chpasswd util to change to pi user password
 passwd pi -l	#Disable the pi account
 
+#Change root password
+echo "-----Hardening root account-----"
+read -p "Enter new password for root" rootpassword
+echo -e "root:$rootpassword" | chpasswd	#use the chpasswd util to change to root user password
+
 #Hardening
-echo "Other hardening actions"
+echo "-----Other hardening actions-----"
 sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd	#Ensure all accounts need to enter their password when carrying out sudo
